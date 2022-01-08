0ac892a80b3fade021d6b9517e74a325055c0105
Master Dev <53656239+iammasterdev1@users.noreply.github.com>
2022-01-08 05:38:36 +0330

    all

--- src/components/home/partner.vue
+++ src/components/home/partner.vue
@@ -1,7 +1,7 @@
 <template>
   <section
-    id="partner"
-    class="bg-color-primary pt-12 xl:pb-52 lg:pb-52 md:pb-36 sm:pb-32 pb-10"
+      id="partner"
+      class="bg-color-primary pt-12 xl:pb-52 lg:pb-52 md:pb-36 sm:pb-32 pb-10"
   >
     <div class="container mx-auto">
       <h1 class="text-5xl font-semibold uppercase mb-24 text-center">
@@ -9,15 +9,15 @@
       </h1>
 
       <div class="grid md:grid-cols-3 sm:grid-cols-2 gap-14">
-        <template v-for="(i, idx) in 6">
-          <div :key="idx" class="text-center">
+        <template v-for="val in data">
+          <div :key="val" class="text-center">
             <div
-              class="mx-auto overflow-hidden rounded-full relative"
-              style="max-width: 192px; height: 192px"
+                class="mx-auto overflow-hidden rounded-full relative"
+                style="max-width: 192px; height: 192px"
             >
               <img
-                src="@/assets/images/img_placeholder.png"
-                class="
+                  :src="val[0].image"
+                  class="
                   w-full
                   h-full
                   absolute
@@ -26,10 +26,10 @@
                   left-0
                   bg-center
                 "
-                alt=""
+                  alt=""
               />
             </div>
-            <h4 class="text-color-dark text-2xl font-medium mt-10">Partner</h4>
+            <h4 class="text-color-dark text-2xl font-medium mt-10">{{val[0].backgroundInformation}}</h4>
           </div>
         </template>
       </div>
@@ -41,8 +41,36 @@
 export default {
   name: "partner",
   data() {
-    return {};
+    return {
+      data: {}
+    };
   },
-};
+  async mounted() {
+
+    // eslint-disable-next-line no-unused-vars
+    let info = this.data
+    let mountTrue
+    mountTrue = await new XMLHttpRequest();
+    mountTrue.open("get" , "https://dyn.trigan.org/api/teammember/getAll?apiKey=ABC123");
+    mountTrue.setRequestHeader("Accept", "application/json");
+    mountTrue.setRequestHeader(
+        "Content-type",
+        "application/x-www-form-urlencoded"
+    )
+    mountTrue.send();
+    mountTrue.onreadystatechange = function () {
+      if (mountTrue.readyState === XMLHttpRequest.DONE) {
+        // eslint-disable-next-line no-unused-vars
+        let response = JSON.parse(mountTrue.response)
+        info.data = response.Data
+
+
+        console.log(info.data)
+
+
+      }
+    }
+  }
+}
 </script>
 
