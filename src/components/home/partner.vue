6d6f361b8134c3a9341a79a2c4624674dfd704a3
aripadrian <aripadrian01@gmail.com>
2022-01-11 22:05:59 +0700

    add shown data

--- src/components/home/partner.vue
+++ src/components/home/partner.vue
@@ -6,21 +6,18 @@
     <div class="container mx-auto">
       <h1 class="text-5xl font-semibold uppercase mb-24 text-center">Team</h1>
 
-      <div class="grid md:grid-cols-4 sm:grid-cols-2 gap-14">
+      <div class="grid md:grid-cols-4 sm:grid-cols-2 gap-4">
         <template v-for="(val, index) in response">
-          <div
-            :key="index"
-            class="text-center relative hover:cursor-pointer"
-            @mouseover="showModal(index)"
-          >
+          <div :key="index" class="relative hover:cursor-pointer">
             <div class="mx-auto">
               <img
+                @mouseover="showModal(index)"
                 :src="val.image"
                 class="h-48 w-48 mx-auto hover:cursor-pointer rounded-full"
                 alt=""
               />
             </div>
-            <div>
+            <div class="text-center">
               <h4 class="text-color-dark text-2xl font-bold mt-4">
                 {{ val.name }}
               </h4>
@@ -30,24 +27,76 @@
               <h4 class="text-color-dark text-sm">
                 {{ val.shortDescription }}
               </h4>
-              <ul class="text-color-dark text-xl font-medium mt-2 flex">
-                <a class="p-2 ml-auto" :href="val.socialLinks.facebook"
-                  ><font-awesome-icon :icon="['fab', 'facebook-square']"
-                /></a>
-                <a class="p-2" :href="val.socialLinks.instagram"
-                  ><font-awesome-icon :icon="['fab', 'instagram-square']"
-                /></a>
-                <a class="p-2 mr-auto" :href="val.socialLinks.youtube"
-                  ><font-awesome-icon :icon="['fab', 'youtube-square']"
-                /></a>
-              </ul>
+              <div>
+                <ul class="text-color-dark text-xl font-medium mt-2 flex">
+                  <a class="p-2 ml-auto" :href="val.socialLinks.facebook"
+                    ><font-awesome-icon :icon="['fab', 'facebook-square']"
+                  /></a>
+                  <a class="p-2" :href="val.socialLinks.instagram"
+                    ><font-awesome-icon :icon="['fab', 'instagram-square']"
+                  /></a>
+                  <a class="p-2 mr-auto" :href="val.socialLinks.youtube"
+                    ><font-awesome-icon :icon="['fab', 'youtube-square']"
+                  /></a>
+                </ul>
+              </div>
             </div>
 
             <div
               v-if="index === openedModal"
-              class="absolute mt-56 z-40 top-0 border left-0 w-full p-4 h-72 rounded-xl bg-color-primary shadow"
+              class="absolute mt-48 z-40 top-0 left-0 w-full p-4 h-auto rounded-xl bg-white shadow"
             >
-              tes
+              <svg
+                class="-mt-8 mx-auto"
+                width="22"
+                height="18"
+                viewBox="0 0 22 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M11 0L21.3923 18H0.607696L11 0Z" fill="white" />
+              </svg>
+
+              <div
+                class="flex border-b mt-6 pb-4 border-color-primary inline-block gap-2"
+              >
+                <img
+                  :src="val.image"
+                  class="h-12 w-12 object-cover rounded-full"
+                  alt=""
+                />
+                <div>
+                  <h3 class="block capitalize font-bold">
+                    {{ val.title }}
+                  </h3>
+                  <div
+                    class="text-color-dark text-xl font-medium flex inline-block gap-2"
+                  >
+                    <a class="" :href="val.socialLinks.facebook"
+                      ><font-awesome-icon :icon="['fab', 'facebook-square']"
+                    /></a>
+                    <a class="" :href="val.socialLinks.instagram"
+                      ><font-awesome-icon :icon="['fab', 'instagram-square']"
+                    /></a>
+                    <a class="" :href="val.socialLinks.youtube"
+                      ><font-awesome-icon :icon="['fab', 'youtube-square']"
+                    /></a>
+                  </div>
+                </div>
+              </div>
+              <p class="text-left text-sm mt-4">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Numquam officiis error placeat cupiditate ab consectetur unde
+                voluptatibus, debitis minima ratione recusandae corporis
+                consequuntur eius labore. Enim inventore reprehenderit
+                temporibus quas.
+                <br />
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Numquam officiis error placeat cupiditate ab consectetur unde
+                voluptatibus, debitis minima ratione recusandae corporis
+                consequuntur eius labore. Enim inventore reprehenderit
+                temporibus quas.
+              </p>
             </div>
           </div>
         </template>
@@ -64,7 +113,7 @@ export default {
   data() {
     return {
       response: {},
-      openedModal: null,
+      openedModal: 1,
     };
   },
   methods: {
