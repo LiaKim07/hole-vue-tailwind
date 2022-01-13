50a91f70d4441e277f6ffad2a720f671b3cddb97
ariasrori <ariasrori43@gmail.com>
2022-01-13 11:16:27 +0700

    fix the the teams animation

--- src/components/home/partner.vue
+++ src/components/home/partner.vue
@@ -6,14 +6,17 @@
     <div class="container mx-auto">
       <h1 class="text-5xl font-semibold uppercase mb-24 text-center">Team</h1>
 
-      <div class="grid md:grid-cols-4 sm:grid-cols-2 gap-4">
+      <div class="grid md:grid-cols-4 sm:grid-cols-2 gap-4 relative">
         <template v-for="(val, index) in response">
-          <div :key="index" class="relative hover:cursor-pointer">
+          <div :key="index" class="cursor-pointer h-max duration-700" 
+            @click="(new Date() - hoverDate) > 700 ? showAdditionalInfo(index, !isOpenAdditionalInfo)  : ''" 
+            @mouseenter="showAdditionalInfo(index, true)" 
+            @mouseleave="showAdditionalInfo(index, false)"
+            :style="{ 'margin-bottom' : additionalInfoIndexActive === index ? additionalInfoMaxHeight : 0 }">
             <div class="mx-auto">
               <img
-                @mouseover="showModal(index)"
                 :src="val.image"
-                class="h-48 w-48 mx-auto hover:cursor-pointer rounded-full"
+                class="h-48 w-48 mx-auto cursor-pointer rounded-full"
                 alt=""
               />
             </div>
@@ -43,76 +46,79 @@
             </div>
 
             <div
-              v-if="index === openedModal"
-              class="absolute mt-48 z-40 top-0 left-0 w-full p-4 h-auto rounded-xl bg-white shadow"
+              :ref="`additionalInfo${index}`"
+              @click="additionalInfoClicked($event, index)"
+              class="absolute left-0 right-0 w-full max-h-0 duration-700 cursor-default bg-yellow-400 overflow-hidden rounded-xl bg-white shadow"
             >
-              <svg
-                class="-mt-8 mx-auto"
-                width="22"
-                height="18"
-                viewBox="0 0 22 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M11 0L21.3923 18H0.607696L11 0Z" fill="white" />
-              </svg>
-
-              <div
-                class="flex mt-6 pb-4 border-color-primary inline-block gap-4"
-              >
-                <img
-                  :src="val.image"
-                  class="h-12 w-12 object-cover rounded-full"
-                  alt=""
-                />
-                <div>
-                  <h3 class="block capitalize font-bold">
-                    {{ val.title }}
-                  </h3>
-                  <div
-                    class="text-color-dark text-xl font-medium flex inline-block gap-2"
-                  >
-                    <a class="" :href="val.socialLinks.facebook"
-                      ><font-awesome-icon :icon="['fab', 'facebook-square']"
-                    /></a>
-                    <a class="" :href="val.socialLinks.instagram"
-                      ><font-awesome-icon :icon="['fab', 'instagram-square']"
-                    /></a>
-                    <a class="" :href="val.socialLinks.youtube"
-                      ><font-awesome-icon :icon="['fab', 'youtube-square']"
-                    /></a>
+              <div class="p-4">
+                <svg
+                  class="-mt-8 mx-auto"
+                  width="22"
+                  height="18"
+                  viewBox="0 0 22 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M11 0L21.3923 18H0.607696L11 0Z" fill="white" />
+                </svg>
+                <div
+                  class="flex mt-6 pb-4 border-color-primary inline-block gap-4"
+                >
+                  <img
+                    :src="val.image"
+                    class="h-12 w-12 object-cover rounded-full"
+                    alt=""
+                  />
+                  <div>
+                    <h3 class="block capitalize font-bold">
+                      {{ val.title }}
+                    </h3>
+                    <div
+                      class="text-color-dark text-xl font-medium flex inline-block gap-2"
+                    >
+                      <a class="" :href="val.socialLinks.facebook"
+                        ><font-awesome-icon :icon="['fab', 'facebook-square']"
+                      /></a>
+                      <a class="" :href="val.socialLinks.instagram"
+                        ><font-awesome-icon :icon="['fab', 'instagram-square']"
+                      /></a>
+                      <a class="" :href="val.socialLinks.youtube"
+                        ><font-awesome-icon :icon="['fab', 'youtube-square']"
+                      /></a>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="flex inline-block gap-2 border-b border-color-dark">
-                <div
-                  @click.prevent="isActive = 'bio'"
-                  :class="
-                    isActive === 'bio'
-                      ? 'font-bold p-2 rounded-t-lg bg-color-dark text-white'
-                      : 'p-2 '
-                  "
-                >
-                  Biography
+                <div class="flex inline-block gap-2 border-b border-color-dark">
+                  <div
+                    @click.prevent="isActive = 'bio'"
+                    :class="
+                      isActive === 'bio'
+                        ? 'font-bold p-2 rounded-t-lg bg-color-dark text-white'
+                        : 'p-2 '
+                    "
+                  >
+                    Biography
+                  </div>
+                  <div
+                    @click.prevent="isActive = 'someData'"
+                    :class="
+                      isActive === 'someData'
+                        ? 'font-bold p-2 rounded-t-lg bg-color-dark text-white'
+                        : 'p-2 '
+                    "
+                  >
+                    Some Data
+                  </div>
                 </div>
-                <div
-                  @click.prevent="isActive = 'someData'"
-                  :class="
-                    isActive === 'someData'
-                      ? 'font-bold p-2 rounded-t-lg bg-color-dark text-white'
-                      : 'p-2 '
-                  "
-                >
-                  Some Data
-                </div>
+                <p class="text-left text-sm mt-4" v-if="isActive === 'bio'">
+                  {{ shown }}
+                </p>
+                <p class="text-left text-sm mt-4" v-if="isActive === 'someData'">
+                  {{ text }}
+                </p>
               </div>
-              <p class="text-left text-sm mt-4" v-if="isActive === 'bio'">
-                {{ shown }}
-              </p>
-              <p class="text-left text-sm mt-4" v-if="isActive === 'someData'">
-                {{ text }}
-              </p>
             </div>
+            
           </div>
         </template>
       </div>
@@ -142,13 +148,40 @@ export default {
       shown: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Numquam officiis error placeat cupiditate ab `,
       isActive: "bio",
+      isOpenAdditionalInfo: false,
+      hoverDate: null,
+      additionalInfoMaxHeight: 0,
+      additionalInfoIndexActive: null,
     };
   },
   methods: {
-    showModal(index) {
-      console.log(index);
-      this.openedModal = index;
+    additionalInfoClicked(event, index){
+      event.stopPropagation()
+      let additionalInfo = this.$refs[`additionalInfo${index}`][0]
+      additionalInfo.style.maxHeight = additionalInfo.scrollHeight+'px'
+      this.additionalInfoMaxHeight = additionalInfo.scrollHeight+'px'
     },
+    showAdditionalInfo(index, status){
+      let additionalInfo = this.$refs[`additionalInfo${index}`][0]
+      if (status) {
+        this.hoverDate = new Date()
+        this.isOpenAdditionalInfo = true
+        additionalInfo.style.maxHeight = additionalInfo.scrollHeight+'px'
+        additionalInfo.style.display = 'block'
+        this.additionalInfoMaxHeight = additionalInfo.scrollHeight+'px'
+        this.additionalInfoIndexActive = index
+      }else{
+        this.hoverDate = false
+        this.isOpenAdditionalInfo = false
+        additionalInfo.style.maxHeight = '0px'
+        this.additionalInfoMaxHeight = 0
+        this.additionalInfoIndexActive = null
+      }
+    },
+    // showModal(index) {
+    //   console.log(index);
+    //   this.openedModal = index;
+    // },
     hideModal() {
       this.$emit("closeModal");
     },
