25e8248e5d913689b2a03ae3a82a139a5df64882
LiaKim07 <kimdubu07lia@gmail.com>
2022-01-16 10:27:23 +0900

    responsive-svg-not-covered

--- src/components/home/hero.vue
+++ src/components/home/hero.vue
@@ -15,12 +15,12 @@
     <img src="@/assets/svg/circle_2.svg" alt="" />
     <img src="@/assets/svg/circle_3.svg" alt="" />
     <img src="@/assets/svg/logo.svg" alt="" /> -->
-    <div class="flex w-full h-full items-center md:w-1/2 md:ml-20 lg:ml-20 lg:mt-40 md:mt-40">
+    <div class="flex w-full h-full lg:items-center md:items-center md:w-1/2 md:ml-20 lg:ml-20 lg:mt-40 md:mt-40">
       <logo-big />
     </div>
     <iframe
       height="45%"
-      class="absolute lg:top-32 md:top-32 sm:top-1/2 md:w-iframe-video-width lg:w-iframe-video-width sm:left-0 lg:left-1/2 md:left-1/2 sm:w-full"
+      class="absolute lg:top-32 md:top-32 sm:top-3/4 md:w-iframe-video-width lg:w-iframe-video-width sm:left-0 lg:left-1/2 md:left-1/2 sm:w-full"
       src="https://www.youtube.com/embed/ewQO72rKlIw"
     >
     </iframe>
